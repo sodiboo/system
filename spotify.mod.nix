e11169b81f0078ddbf1581b92a70934d1e7ecc14
sodiboo <git@sodi.boo>
2024-05-25 13:50:32 +0200

    prepare spotifyd for sops
    can use --username-cmd and --password-cmd easily now
    
    password-cmd is also available in config but username-cmd doesn't seem
    to be, and i consider the username for spotify to also be a secret

--- spotify.mod.nix
+++ spotify.mod.nix
@@ -1,15 +1,40 @@
 {secrets, ...}: {
   personal.home_modules = [
     ({
+      config,
       pkgs,
       lib,
       ...
+    }: let
+      cfg = config.services.spotifyd;
+      tomlFormat = pkgs.formats.toml {};
+      configFile = tomlFormat.generate "spotifyd.conf" cfg.settings;
+    in {
+      systemd.user.services.spotifyd = lib.mkForce {
+        Unit = {
+          Description = "spotify daemon";
+          Documentation = "https://github.com/Spotifyd/spotifyd";
+        };
+
+        Install.WantedBy = ["default.target"];
+
+        Service = {
+          ExecStart = "${cfg.package}/bin/spotifyd ${lib.escapeShellArgs [
+            "--no-daemon"
+            "--username" secrets.spotify-username
+            "--password" secrets.spotify-password
+            "--config-path" configFile
+          ]}";
+          Restart = "always";
+          RestartSec = 12;
+        };
+      };
+    })
+    ({
+      pkgs,
+      ...
     }: {
       services.spotifyd.enable = true;
-      services.spotifyd.settings.global = {
-        username = secrets.spotify-username;
-        password = secrets.spotify-password;
-      };
 
       home.packages = with pkgs; [
         spotify-player
