996057e3cbf0b34ec256678e29aa9ae94e00b537
sodiboo <git@sodi.boo>
2024-06-28 22:29:26 +0200

    add ssh key for sodium->oxygen

--- ssh.mod.nix
+++ ssh.mod.nix
@@ -4,7 +4,8 @@
       services.openssh.enable = true;
       services.openssh.passwordAuthentication = false;
       users.users.sodiboo.openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN8eWTRBpEegdAdTkPeBJXmyi7o2WQFL3mdWf2FRoXdo sodiboo@contabo-vps"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN8eWTRBpEegdAdTkPeBJXmyi7o2WQFL3mdWf2FRoXdo sodiboo@lithium"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIExB5YOySRzfOx8RUZAcV8yh8SBwq5lAsQD1df8UfrHw sodiboo@sodium"
       ];
     }
   ];
@@ -18,6 +19,20 @@
             HostName vps.sodi.boo
             User sodiboo
             IdentityFile ~/.ssh/contabo-vps-2024-05
+        '';
+      };
+    }
+  ];
+
+  sodium.home_modules = [
+    {
+      programs.ssh = {
+        enable = true;
+        extraConfig = ''
+          Host oxygen
+            HostName vps.sodi.boo
+            User sodiboo
+            IdentityFile ~/.ssh/contabo-vps-2024-06
         '';
       };
     }
