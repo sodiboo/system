c2e8a99d99c8d64a286034829193419f9b054cd8
sodiboo <git@sodi.boo>
2024-06-28 22:18:05 +0200

    add reverse shell helper

--- dev.mod.nix
+++ dev.mod.nix
@@ -1,6 +1,28 @@
 {
   universal.modules = [
-    ({pkgs, ...}: {
+    ({
+      pkgs,
+      lib,
+      ...
+    }: {
+      nixpkgs.overlays = [
+        (final: prev: {
+          # Getting a shell to run in annoying, non-tty nested environments
+          # e.g. for debugging stuff inside of xwayland-run without using xterm
+          # or in the environment of `niri msg action spawn` without needing to spawn a graphical window
+          sodi-revshell = final.symlinkJoin {
+            name = "revshell";
+            paths = [
+              (final.writeScriptBin "revsh-attach" ''
+                ${lib.getExe final.socat} file:$(tty),raw,echo=0 tcp:127.0.0.1:"$1"
+              '')
+              (final.writeScriptBin "revsh-here" ''
+                ${lib.getExe final.socat} exec:"sh -lic $SHELL",pty,stderr,setsid,sigint,sane tcp-listen:"$1"
+              '')
+            ];
+          };
+        })
+      ];
       environment.systemPackages = with pkgs; [
         chase
         tldr
@@ -19,6 +41,7 @@
         bc
         dust
         moreutils
+        sodi-revshell
       ];
     })
     {
