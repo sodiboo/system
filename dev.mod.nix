a58d5e9f5df3eb8cbf4f9b20f148ed9c9850f282
sodiboo <git@sodi.boo>
2024-08-16 14:55:55 +0200

    remove .envrc, .direnv from global ignores
    these belong in .git/info/exclude for the relevant repo (niri)

--- dev.mod.nix
+++ dev.mod.nix
@@ -71,7 +71,7 @@
 
       programs.direnv.enable = true;
       programs.direnv.nix-direnv.enable = true;
-      programs.git.ignores = ["**/.envrc" "**/.direnv"];
+      programs.git.ignores = [];
     })
   ];
 }
