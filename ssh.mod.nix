cf66e674ecc4ede8cc822545f5f17129c8a4058e
sodiboo <git@sodi.boo>
2024-08-18 19:50:45 +0200

    add nitrogen to ssh config and make it universal across wireguard

--- ssh.mod.nix
+++ ssh.mod.nix
@@ -1,19 +1,8 @@
 {
-  oxygen.modules = [
+  universal.modules = [
     {
       services.openssh.enable = true;
-      services.openssh.passwordAuthentication = false;
-      users.users.sodiboo.openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN8eWTRBpEegdAdTkPeBJXmyi7o2WQFL3mdWf2FRoXdo sodiboo@lithium"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIExB5YOySRzfOx8RUZAcV8yh8SBwq5lAsQD1df8UfrHw sodiboo@sodium"
-      ];
-    }
-  ];
-
-  iridium.modules = [
-    {
-      services.openssh.enable = true;
-      services.openssh.passwordAuthentication = false;
+      services.openssh.settings.PasswordAuthentication = false;
       users.users.sodiboo.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN8eWTRBpEegdAdTkPeBJXmyi7o2WQFL3mdWf2FRoXdo sodiboo@lithium"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIExB5YOySRzfOx8RUZAcV8yh8SBwq5lAsQD1df8UfrHw sodiboo@sodium"
@@ -34,6 +23,32 @@
             HostName iridium.lan
             User sodiboo
             IdentityFile ~/.ssh/id_ed25519-2024-06
+          Host nitrogen
+            HostName nitrogen.wg
+            User sodiboo
+            IdentityFile ~/.ssh/id_ed25519-2024-06
+        '';
+      };
+    }
+  ];
+
+  nitrogen.home_modules = [
+    {
+      programs.ssh = {
+        enable = true;
+        extraConfig = ''
+          Host oxygen
+            HostName oxygen.wg
+            User sodiboo
+            IdentityFile ~/.ssh/id_ed25519-2024-08
+          Host iridium
+            HostName iridium.wg
+            User sodiboo
+            IdentityFile ~/.ssh/id_ed25519-2024-08
+          Host sodium
+            HostName sodium.wg
+            User sodiboo
+            IdentityFile ~/.ssh/id_ed25519-2024-08
         '';
       };
     }
