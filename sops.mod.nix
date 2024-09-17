5df62b90fe5afc1b4e3d3d3177b41f5f18d06f61
sodiboo <git@sodi.boo>
2024-09-17 21:50:14 +0200

    lol weird unicode thingy

--- sops.mod.nix
+++ sops.mod.nix
@@ -5,7 +5,7 @@
       sops.defaultSopsFile = ./secrets.yaml;
       sops.defaultSopsFormat = "yaml";
 
-      # sync ̃~/.ssh/sops out-of-band
+      # sync ~/.ssh/sops out-of-band
       # ssh-to-age -private-key -i ~/.ssh/sops > ~/.config/sops/age/keys.txt
       sops.age.keyFile = "/home/sodiboo/.config/sops/age/keys.txt";
 
