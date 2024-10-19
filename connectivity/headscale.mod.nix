6c7292d1e55c920aa762483423c207efe50bc143
sodiboo <git@sodi.boo>
2024-10-19 02:12:12 +0200

    new headscale config who dis

--- connectivity/headscale.mod.nix
+++ connectivity/headscale.mod.nix
@@ -6,15 +6,14 @@
         port = 3004;
         settings = {
           server_url = "https://vpn.sodi.boo";
+          dns.base_domain = "tail";
 
           log.level = "warn";
           logtail.enabled = false;
           metrics_listen_addr = "127.0.0.1:3005";
 
-          ip_prefixes = [
-            "100.64.0.0/10"
-            "fd7a:115c:a1e0::/48"
-          ];
+          prefixes.v4 = "100.64.0.0/10";
+          prefixes.v6 = "fd7a:115c:a1e0::/48";
 
           derp.server = {
             enable = true;
