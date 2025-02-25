f3a5c8b5c5888d05e47d3547b51396f97ba4a69c
sodiboo <git@sodi.boo>
2025-02-25 04:43:41 +0100

    lol remove this old workaround

--- workarounds.mod.nix
+++ workarounds.mod.nix
@@ -1,13 +1,2 @@
-{nixpkgs-stable, ...}: {
-  universal.modules = [
-    ({pkgs, ...}: {
-      nixpkgs.overlays = [
-        (
-          final: prev: {
-            inherit (nixpkgs-stable.legacyPackages.${pkgs.stdenv.system}) glfw-wayland-minecraft;
-          }
-        )
-      ];
-    })
-  ];
+{...}: {
 }
