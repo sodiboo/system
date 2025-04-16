50abd2f7d0f161b4996d823eb6a7e585753fec65
sodiboo <git@sodi.boo>
2025-04-16 20:57:01 +0200

    upgrade sodium back to 6.12; i disabled iGPU lol
    https://github.com/NixOS/nixpkgs/issues/382612#issuecomment-2734556441

--- workarounds.mod.nix
+++ workarounds.mod.nix
@@ -1,10 +1,4 @@
 {...}: {
-  sodium.modules = [
-    ({pkgs, ...}: {
-      boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_6;
-    })
-  ];
-
   nitrogen.modules = [
     ({pkgs, ...}: {
       boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_1;
