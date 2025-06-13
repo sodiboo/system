34299ef218591bc0e1e4f8fa1b052f4b93727427
sodiboo <git@sodi.boo>
2025-06-13 15:07:42 +0200

    fix the meilisearch pin

--- sharkey/import.mod.nix
+++ sharkey/import.mod.nix
@@ -5,6 +5,7 @@
         (final: prev: {
           sharkey = final.callPackage ./package.nix {};
           meilisearch = nixpkgs-with-meilisearch-at-1-8-3.legacyPackages.x86_64-linux.meilisearch;
+          meilisearch_1_11 = final.meilisearch; # <-- it uses this package because of an old stateVersion. i'll upgrade at some point.
         })
       ];
     }
