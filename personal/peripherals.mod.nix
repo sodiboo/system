619af914170aaadc80f54dcde156205b7e35ae93
sodiboo <git@sodi.boo>
2025-06-14 19:42:21 +0200

    cleanup this thing; also those are fine in vm actually

--- personal/peripherals.mod.nix
+++ personal/peripherals.mod.nix
@@ -8,30 +8,20 @@
     services.gvfs.enable = true;
     services.devmon.enable = true;
   };
-  sodium.imports = [
-    (
-      {
-        pkgs,
-        lib,
-        config,
-        ...
-      }:
-      lib.mkIf (!config.is-virtual-machine) {
-        hardware.wooting.enable = true;
-        users.users.sodiboo.extraGroups = [ "input" ];
 
-        environment.systemPackages = with pkgs; [
-          openrgb-with-all-plugins
-          openrazer-daemon
-          polychromatic
-        ];
-      }
-    )
-
+  sodium =
+    { pkgs, ... }:
     {
+      hardware.wooting.enable = true;
+      users.users.sodiboo.extraGroups = [ "input" ];
+
+      environment.systemPackages = with pkgs; [
+        openrgb-with-all-plugins
+        openrazer-daemon
+        polychromatic
+      ];
       hardware.i2c.enable = true;
-    }
-  ];
+    };
 
   nitrogen = {
     services.upower.enable = true;
