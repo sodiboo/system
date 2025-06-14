4d282c1fadb65980cc30cc9acef2789b88734a95
sodiboo <git@sodi.boo>
2025-06-14 19:41:14 +0200

    make direnv silent. oh my god shut the fuck up.

--- nix/dev.mod.nix
+++ nix/dev.mod.nix
@@ -15,23 +15,23 @@
           nixos-rebuild = prev.nixos-rebuild.override {
             nix = prev.nix-monitored;
           };
-          nix-direnv = final.runCommand "nix-direnv-monitored" { } ''
-            cp -R ${
-              prev.nix-direnv.override {
-                # Okay, so what's happening here is that `nix-direnv` doesn't just use `nix` from PATH.
-                # However, it also doesn't use `nix` from nix store. It uses both.
-                # So what i'm doing here, is setting the "fallback path" to nix, being nix-monitored.
-                nix = prev.nix-monitored;
-              }
-            } $out
-            chmod -R +w $out
-            ${
-              # And then, i'm replacing the command that it uses to find nix with `false`.
-              # This makes it think there's no nix in PATH, and it will use the fallback path.
-              # And voila, i get nom output in direnv.
-              "sed -i 's/command -v nix/false/' $out/share/nix-direnv/direnvrc"
-            }
-          '';
+          # nix-direnv = final.runCommand "nix-direnv-monitored" { } ''
+          #   cp -R ${
+          #     prev.nix-direnv.override {
+          #       # Okay, so what's happening here is that `nix-direnv` doesn't just use `nix` from PATH.
+          #       # However, it also doesn't use `nix` from nix store. It uses both.
+          #       # So what i'm doing here, is setting the "fallback path" to nix, being nix-monitored.
+          #       nix = prev.nix-monitored;
+          #     }
+          #   } $out
+          #   chmod -R +w $out
+          #   ${
+          #     # And then, i'm replacing the command that it uses to find nix with `false`.
+          #     # This makes it think there's no nix in PATH, and it will use the fallback path.
+          #     # And voila, i get nom output in direnv.
+          #     "sed -i 's/command -v nix/false/' $out/share/nix-direnv/direnvrc"
+          #   }
+          # '';
 
           nixmon = final.runCommand "nixmon" { } ''
             mkdir -p $out/bin
@@ -53,6 +53,7 @@
         ];
 
         programs.direnv.enable = true;
+        programs.direnv.silent = true;
         programs.direnv.nix-direnv.enable = true;
       };
     };
