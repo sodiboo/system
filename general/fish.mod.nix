9fa4d75c9a6a93fff87f7cdbf98c1a3e8ee06551
sodiboo <git@sodi.boo>
2025-07-08 14:28:58 +0200

    add fish-completions-sync for better dev shell completions

--- general/fish.mod.nix
+++ general/fish.mod.nix
@@ -33,6 +33,18 @@
                 eza = "eza --long --all --icons --time-style long-iso";
                 "@" = "kitten ssh";
               };
+
+              plugins = [
+                {
+                  name = "fish-completions-sync";
+                  src = pkgs.fetchFromGitHub {
+                    owner = "pfgray";
+                    repo = "fish-completion-sync";
+                    rev = "4f058ad2986727a5f510e757bc82cbbfca4596f0";
+                    sha256 = "sha256-kHpdCQdYcpvi9EFM/uZXv93mZqlk1zCi2DRhWaDyK5g=";
+                  };
+                }
+              ];
             };
 
             powerline-go = {
