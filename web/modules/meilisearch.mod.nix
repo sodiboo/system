139c29dff52f2bbd3574561902cb493a1f6865d4
sodiboo <git@sodi.boo>
2025-07-29 07:58:29 +0200

    meilisearch: harden
    it's literally just https://github.com/NixOS/nixpkgs/pull/427768

--- web/modules/meilisearch.mod.nix
+++ web/modules/meilisearch.mod.nix
@@ -19,6 +19,48 @@
             http_addr = "${cfg.listenAddress}:${toString cfg.listenPort}";
             no_analytics = lib.mkDefault true;
           };
+
+        systemd.services.meilisearch.serviceConfig = {
+
+          ProtectSystem = "strict";
+          ProtectHome = true;
+          ProtectClock = true;
+          ProtectHostname = true;
+          ProtectKernelLogs = true;
+          ProtectKernelModules = true;
+          ProtectKernelTunables = true;
+          ProtectControlGroups = true;
+          PrivateTmp = true;
+          PrivateMounts = true;
+          PrivateUsers = true;
+          PrivateDevices = true;
+          RestrictRealtime = true;
+          RestrictNamespaces = true;
+          RestrictSUIDSGID = true;
+          LockPersonality = true;
+          MemoryDenyWriteExecute = true;
+
+          ProcSubset = "pid";
+          ProtectProc = "invisible";
+
+          NoNewPrivileges = true;
+
+          RestrictAddressFamilies = [
+            # Meilisearch does not support listening on AF_UNIX sockets,
+            # so we currently restrict it to only AF_INET and AF_INET6.
+            "AF_INET"
+            "AF_INET6"
+          ];
+
+          CapabilityBoundingSet = "";
+          SystemCallArchitectures = "native";
+          SystemCallFilter = [
+            "@system-service"
+            "~@privileged @resources"
+          ];
+
+          UMask = "0077";
+        };
       };
     };
 }
