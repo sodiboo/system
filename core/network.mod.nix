6582b737dc48ed25ff171bc3e45201921573ebcb
sodiboo <git@sodi.boo>
2025-08-28 01:38:33 +0200

    put sodium on networkd
    nitrogen will do the same. eventually. i think.

--- core/network.mod.nix
+++ core/network.mod.nix
@@ -34,9 +34,14 @@
       };
     };
 
-  personal = {
+  nitrogen = {
     # should move to networkd eventually
     networking.networkmanager.enable = true;
     users.users.sodiboo.extraGroups = [ "networkmanager" ];
+  };
+
+  sodium = {
+    systemd.network.enable = true;
+    networking.useNetworkd = true;
   };
 }
