f69787b45e1c24618710862c960ed0ecfdc435ae
sodiboo <git@sodi.boo>
2025-08-28 01:37:43 +0200

    move oxygen networking stuff to network.mod.nix

--- core/hardware.mod.nix
+++ core/hardware.mod.nix
@@ -164,35 +164,5 @@ in
       ];
       boot.initrd.kernelModules = [ "nvme" ];
     }
-    (
-      { lib, config, ... }:
-      {
-        options.public-ipv4 = lib.mkOption {
-          type = lib.types.str;
-        };
-        options.public-ipv6 = lib.mkOption {
-          type = lib.types.str;
-        };
-        config = {
-          # These should match `vps.sodi.boo` DNS records.
-          # All other domains are (flattened) CNAMEs to `vps.sodi.boo`.
-          public-ipv4 = "85.190.241.69";
-          public-ipv6 = "2a02:c202:2189:7245::1";
-
-          networking = {
-
-            enableIPv6 = true;
-            defaultGateway6.address = "fe80::1";
-            defaultGateway6.interface = "ens18";
-            interfaces.ens18.ipv6.addresses = [
-              {
-                address = config.public-ipv6;
-                prefixLength = 64;
-              }
-            ];
-          };
-        };
-      }
-    )
   ])
 ]
--- core/network.mod.nix
+++ core/network.mod.nix
@@ -4,6 +4,36 @@
     networking.useNetworkd = true;
   };
 
+  oxygen =
+    { lib, config, ... }:
+    {
+      options.public-ipv4 = lib.mkOption {
+        type = lib.types.str;
+      };
+      options.public-ipv6 = lib.mkOption {
+        type = lib.types.str;
+      };
+      config = {
+        # These should match `vps.sodi.boo` DNS records.
+        # All other domains are (flattened) CNAMEs to `vps.sodi.boo`.
+        public-ipv4 = "85.190.241.69";
+        public-ipv6 = "2a02:c202:2189:7245::1";
+
+        networking = {
+
+          enableIPv6 = true;
+          defaultGateway6.address = "fe80::1";
+          defaultGateway6.interface = "ens18";
+          interfaces.ens18.ipv6.addresses = [
+            {
+              address = config.public-ipv6;
+              prefixLength = 64;
+            }
+          ];
+        };
+      };
+    };
+
   personal = {
     # should move to networkd eventually
     networking.networkmanager.enable = true;
