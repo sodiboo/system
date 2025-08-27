82a8049282f114567b83a8fcef93fdae57034b54
sodiboo <git@sodi.boo>
2025-08-28 01:35:34 +0200

    move networkmanager to network.mod.nix

--- core/network.mod.nix
+++ core/network.mod.nix
@@ -3,4 +3,10 @@
     systemd.network.enable = true;
     networking.useNetworkd = true;
   };
+
+  personal = {
+    # should move to networkd eventually
+    networking.networkmanager.enable = true;
+    users.users.sodiboo.extraGroups = [ "networkmanager" ];
+  };
 }
--- personal/wireless.mod.nix
+++ personal/wireless.mod.nix
@@ -2,9 +2,6 @@
   personal =
     { pkgs, ... }:
     {
-      networking.networkmanager.enable = true;
-      users.users.sodiboo.extraGroups = [ "networkmanager" ];
-
       hardware.bluetooth.enable = true;
 
       environment.systemPackages = with pkgs; [
