836174ee2a849bc6754c852b7f528a2973f1c8c0
sodiboo <git@sodi.boo>
2025-08-27 06:49:25 +0200

    harden ssh auth a little bit more

--- core/ssh.mod.nix
+++ core/ssh.mod.nix
@@ -1,14 +1,23 @@
 {
   universal = {
-    services.openssh.enable = true;
-    services.openssh.settings.PasswordAuthentication = false;
+    services.openssh = {
+      enable = true;
+      settings = {
+        AuthenticationMethods = "publickey";
+        PermitRootLogin = "no";
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+      };
 
-    services.openssh.hostKeys = [
-      {
-        path = "/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-    ];
+      authorizedKeysInHomedir = false;
+
+      hostKeys = [
+        {
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
+    };
 
     environment.persistence."/nix/persist".files = [ "/etc/ssh/ssh_host_ed25519_key" ];
   };
