85b7ead36c8ebd08c14c80334601e20eb17ece73
sodiboo <git@sodi.boo>
2025-11-01 21:59:15 +0100

    rebracket fish config a little

--- shell/fish.mod.nix
+++ shell/fish.mod.nix
@@ -15,27 +15,25 @@
       home-shortcut = {
         home.persistence."/nix/persist".files = [ ".local/share/fish/fish_history" ];
 
-        programs = {
-          fish = {
-            enable = true;
-            shellAliases = {
-              nix-shell = "nix-shell --run fish";
-              eza = "eza --long --all --icons --time-style long-iso";
-              "@" = "kitten ssh";
-            };
-
-            plugins = [
-              {
-                name = "fish-completions-sync";
-                src = pkgs.fetchFromGitHub {
-                  owner = "pfgray";
-                  repo = "fish-completion-sync";
-                  rev = "4f058ad2986727a5f510e757bc82cbbfca4596f0";
-                  sha256 = "sha256-kHpdCQdYcpvi9EFM/uZXv93mZqlk1zCi2DRhWaDyK5g=";
-                };
-              }
-            ];
+        programs.fish = {
+          enable = true;
+          shellAliases = {
+            nix-shell = "nix-shell --run fish";
+            eza = "eza --long --all --icons --time-style long-iso";
+            "@" = "kitten ssh";
           };
+
+          plugins = [
+            {
+              name = "fish-completions-sync";
+              src = pkgs.fetchFromGitHub {
+                owner = "pfgray";
+                repo = "fish-completion-sync";
+                rev = "4f058ad2986727a5f510e757bc82cbbfca4596f0";
+                sha256 = "sha256-kHpdCQdYcpvi9EFM/uZXv93mZqlk1zCi2DRhWaDyK5g=";
+              };
+            }
+          ];
         };
       };
     };
