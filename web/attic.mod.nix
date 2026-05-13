195a818bc8ca7c3e4a26aeaba44ad68309354fa2
sodiboo <git@sodi.boo>
2026-05-13 18:02:36 +0200

    host a binary cache!!! attic babeyyyy
    this is mostly just for speeding up forgejo actions

---
+++ web/attic.mod.nix
@@ -0,0 +1,44 @@
+{
+  oxygen =
+    { lib, config, ... }:
+    {
+      caddy.sites."loft.computers.gay".routes = [
+        {
+          terminal = true;
+          handle = [
+            {
+              handler = "reverse_proxy";
+              upstreams = [
+                { dial = "${config.services.atticd.settings.listen}"; }
+              ];
+            }
+          ];
+        }
+      ];
+
+      sops.secrets."attic-server-token" = { };
+
+      sops.templates.atticd-env.content = ''
+        ATTIC_SERVER_TOKEN_RS256_SECRET_BASE64="${config.sops.placeholder.attic-server-token}"
+      '';
+
+      services.atticd = {
+        enable = true;
+
+        environmentFile = config.sops.templates.atticd-env.path;
+
+        settings = {
+          listen = "127.0.0.1:2588";
+          database.url = "postgresql:///atticd?host=/run/postgresql";
+        };
+      };
+
+      services.postgresql.ensureDatabases = [ "atticd" ];
+      services.postgresql.ensureUsers = [
+        {
+          name = "atticd";
+          ensureDBOwnership = true;
+        }
+      ];
+    };
+}
