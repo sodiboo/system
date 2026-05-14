4d616a611190c0330f53bea715f6b4e9bffd680b
sodiboo <git@sodi.boo>
2026-05-14 19:29:30 +0200

    patch bluez to fix connectivity regression from 5.86

--- personal/pipewire.mod.nix
+++ personal/pipewire.mod.nix
@@ -56,6 +56,20 @@
         ];
       };
     }
+    (
+      { pkgs, ... }:
+      {
+        hardware.bluetooth.package = pkgs.bluez.overrideAttrs (prevAttrs: {
+          patches = prevAttrs.patches ++ [
+            (pkgs.fetchpatch {
+              name = "fix-bluetooth-connect-order.patch";
+              url = "https://github.com/bluez/bluez/commit/066a164a524e4983b850f5659b921cb42f84a0e0.patch";
+              hash = "sha256-I1WoBJZEZJ05hwGuksp52I4FLJ+jbG9t7U2sLTFmU0w=";
+            })
+          ];
+        });
+      }
+    )
   ];
 
   personal.home-shortcut =
