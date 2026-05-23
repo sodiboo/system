9bc3c1744807db0de111c5e6d9b84e77c12ef264
sodiboo <git@sodi.boo>
2026-05-24 01:07:21 +0200

    these expired

--- web/unused-domains.mod.nix
+++ web/unused-domains.mod.nix
@@ -7,12 +7,9 @@
           unused-domains = [
             "catboy.rocks"
             "mrrp.ing"
-            "enby.lol"
-            "enby.live"
             "sodi.lol"
             "girlcock.party"
             "yester.gay"
-            "infodumping.place"
           ];
         in
         lib.genAttrs unused-domains (lib.const { routes = config.caddy.routes.unknown; });
